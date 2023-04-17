d014c8860891bcd2f01eb978a356a9e77f0ad829
zzangsally <zzangsally@naver.com>
2023-04-17 16:57:16 +0900

    Calendar using FullCalendar Package

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calender";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -35,12 +35,12 @@ function App() {
               <Route path="/hospital_reservation" element={<Contacts />} />
               <Route path="/doctor" element={<Invoices />} />
               <Route path="/hospital_newreservation" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
               {/* <Route path="/geo" element={<Geography />} /> */}
-              {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
